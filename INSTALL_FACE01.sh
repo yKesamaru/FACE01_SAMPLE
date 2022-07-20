6908c1679765458e161a6dc4ddbcfeeb8e4b6dbb
yKesamaru <y.kesamaru@tokai-kaoninsho.com>
2022-07-20 17:59:06 +0900

    fix: INSTALL_FACE01.sh

--- INSTALL_FACE01.sh
+++ INSTALL_FACE01.sh
@@ -6,42 +6,46 @@ set -x
 # THIS IS *ONLY* USE FOR UBUNTU *20.04*
 # -----------------------------------------------------------------
 
-
-apt update && sudo apt upgrade -y
-apt install -y build-essential
-apt install -y cmake
-apt install -y ffmpeg
-apt install -y fonts-mplus
-apt install -y git
-apt install -y libavcodec-dev
-apt install -y libavformat-dev
-apt install -y libswscale-dev
-apt install -y libx11-dev
-apt install -y python3-dev
-apt install -y python3-tk
-apt install -y python3-venv
-apt install -y wget
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y build-essential
+sudo apt install -y cmake
+sudo apt install -y ffmpeg
+sudo apt install -y fonts-mplus
+sudo apt install -y git
+sudo apt install -y libavcodec-dev
+sudo apt install -y libavformat-dev
+sudo apt install -y libswscale-dev
+sudo apt install -y libx11-dev
+sudo apt install -y python3-dev
+sudo apt install -y python3-tk
+sudo apt install -y python3-venv
+sudo apt install -y wget
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 
-apt update && sudo apt upgrade -y
-ubuntu-drivers autoinstall
-apt install -y cuda
-apt install -y libcublas
-apt install -y libcudnn8
-apt install -y libcudnn8-dev
-apt install -y liblapack-dev
-apt install -y libopenblas-dev
-apt install -y nvidia-cuda-toolkit
 
-apt autoremove -y
+sudo apt update && sudo apt upgrade -y
+sudo ubuntu-drivers autoinstall
+sudo apt install -y cuda
+sudo apt install -y libcublas
+# E: パッケージ libcublas が見つかりません
+sudo apt install -y libcudnn8
+sudo apt install -y libcudnn8-dev
+sudo apt install -y liblapack-dev
+sudo apt install -y libopenblas-dev
+sudo apt install -y nvidia-cuda-toolkit
 
-exit
+sudo apt autoremove -y
 
-cat << EOS
+# DEBUG ----
+# echo "$USERNAME"
+# exit
+#  ---------
+
+cat << EOS >> ~/.bashrc
 export PATH="/usr/local/cuda/bin/:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda/lib64/:$LD_LIBRARY_PATH"
 export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
@@ -49,8 +53,9 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PAT
 EOS
 source ~/.bashrc
 
-mkdir -p ~/bin/DIST/
-cd ~/bin/DIST/
+git clone https://github.com/yKesamaru/FACE01_SAMPLE.git
+
+cd ./FACE01_SAMPLE
 
 python3 -m venv ./
 source bin/activate
@@ -61,16 +66,12 @@ pip install -U wheel
 pip install -U setuptools
 pip install -r requirements.txt
 
-wget http://dlib.net/files/dlib-19.24.tar.bz2
 tar -jxvf dlib-19.24.tar.bz2
 cd dlib-19.24
 # `--clean` see bellow
 # [Have you done sudo python3 setup.py install --clean yet?](https://github.com/davisking/dlib/issues/1686#issuecomment-471509357)
 python3 setup.py install --clean
-
 cd ../
-
-git clone https://github.com/yKesamaru/FACE01_SAMPLE.git .
 
 python CALL_FACE01.py
 
